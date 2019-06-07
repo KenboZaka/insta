8f82cad88bc5aff14daee6466675e301d010b812
KenboZaka <kenbobusiness@gmail.com>
2019-06-07 23:31:51 +0900

    index.blade.php modify for picture

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -33,7 +33,7 @@
                             @if(isset($post->image))
                                 <div class="col-lg-4">
                                    
-                                    <img src="{{asset('storage/images/'.$post->image)}}" width="300px" class="my-3 rounded border">
+                                    <img src="{{$post->image)}}" width="300px" class="my-3 rounded border">
                                 </div>
                                 @else
                                 <div class="col-lg-4 text-center py-5">
